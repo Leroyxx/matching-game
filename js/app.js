94631db2cb5dd79fbfed48dcb75010ab86c117f4
Leroy Haimoff <leroyblergh@gmail.com>
2018-03-14 01:24:59 +0200

    feat: Basic functionality
    Can view and match cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 /*
  * Create a list that holds all of your cards
+ * 1. Diamond | 2. Bicycle | 3. Bomb | 4. Cube
+ * 5. Paperplane | 6. Bolt | 7. Leaf | 8. Anchor
  */
-
+ const cardList = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
 /*
  * Display the cards on the page
@@ -10,21 +12,50 @@
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle function from http://stackoverflow.com/a/2450976, I've explained it to myself with side comments.
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
 
     while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        // return number between 1 and number of cards on cardList
+        randomIndex = Math.floor(Math.random() * currentIndex); // get number between 1 to 15
+        currentIndex -= 1; // we start with 15 as currentIndex (16-1) which is the last item in the array
+        temporaryValue = array[currentIndex]; //what's lying behind card #[currentIndex] is temporaryValue, we save it
+        // because it's gonna get overwritten when we re-assign it.
+        array[currentIndex] = array[randomIndex]; //put whatever card you got randomly in the current index
+        array[randomIndex] = temporaryValue; //put whatever card was in the current index in the random index.
     }
 
     return array;
+
 }
 
+let shufdCardList = shuffle(cardList);
+
+function placeCards() {
+  let cardSpot = 1; //c+cardSpot is the id of the card on the DOM
+  let cardOnDOM;
+  const addSymbolData = card => {cardOnDOM.setAttribute("data-symbol", card)}
+  for (const card of shufdCardList) {
+    cardOnDOM = document.getElementById(`c${cardSpot}`);
+    let cardClass;
+    switch(card) {
+      case 1: cardClass = "fa-diamond"; addSymbolData(card); break;
+      case 2: cardClass = "fa-bicycle"; addSymbolData(card); break;
+      case 3: cardClass = "fa-bomb"; addSymbolData(card); break;
+      case 4: cardClass = "fa-cube"; addSymbolData(card); break;
+      case 5: cardClass = "fa-paper-plane-o"; addSymbolData(card); break;
+      case 6: cardClass = "fa-bolt"; addSymbolData(card); break;
+      case 7: cardClass = "fa-leaf"; addSymbolData(card); break;
+      case 8: cardClass = "fa-anchor"; addSymbolData(card); break;
+    }
+    cardOnDOM.firstElementChild.classList.add(cardClass)
+    cardSpot++;
+  }
+}
+placeCards();
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -36,3 +67,42 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let deck = document.querySelector(".deck");
+let firstCard = 0;
+let secondCard = 0;
+let isEqual = 0;
+let movesNum = 0;
+let moves = document.querySelector('.moves');
+
+function showCard(cardSpot) {
+  movesNum++;
+  moves.textContent = movesNum;
+  if (firstCard === 0) {
+    firstCard = cardSpot;
+    firstCard.classList.add('show');
+    return false;
+  } //keep the card as firstCard if there is no card open yet
+  if (firstCard !== 0 && secondCard === 0) {
+    secondCard = cardSpot;
+    secondCard.classList.add('show');
+    checkEquality(firstCard, secondCard);
+    setTimeout(function() {
+      if (!isEqual) { secondCard.classList.remove('show'); firstCard.classList.remove('show');
+      firstCard = 0; secondCard = 0;
+      }
+      },  1600);
+  }
+  function checkEquality(first, second) {
+    isEqual = ( first.dataset.symbol === second.dataset.symbol ) ? true : 0;
+    if (isEqual) {firstCard = 0; secondCard = 0;}
+  }
+};
+
+
+deck.onclick = function(event) {
+  let target = event.target;
+  let ccSpot = target.closest('li'); //clicked card spot, navigate up the tree to the li parent element
+  if (!ccSpot) return; //if clicked somewhere else inside deck do nothing
+  showCard(ccSpot);
+}
